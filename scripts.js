b42c2dd50a30884dd7deb60102dafc2739aa1844
Franco Baez <francombaez96@gmail.com>
2023-04-10 19:20:28 -0300

    Mejora de card y animacion de imagen de card, optimizacion de fetch(api)

--- scripts.js
+++ scripts.js
@@ -4,20 +4,41 @@ const name = card.querySelector('.name');
 const number = card.querySelector('.number');
 const type = card.querySelector('.type');
 const image = card.querySelector('img');
+const height = card.querySelector('.height');
+const weight = card.querySelector('.weight');
+const description = card.querySelector('.description');
+image.style.opacity = 1;
 
-function getRandomPokemon(){
-    const randomId = Math.floor(Math.random() * 898) + 1; // Genera un id aleatorio entre 1 y 898
-      const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-      fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-          // Actualiza la información de la tarjeta con el Pokemon aleatorio
-          name.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-          number.textContent = `#${data.id.toString().padStart(3, '0')}`;
-          type.textContent = `Type: ${data.types.map(type => type.type.name).join(', ')}`;
-          image.src = data.sprites.front_default;
-          image.alt = data.name;
-})
-.catch(error => console.log(error));
-}
+const getRandomPokemon = async () => {
+  const randomId = Math.floor(Math.random() * 898) + 1;
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+  const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${randomId}`;
+
+  try {
+    const [pokemonResponse, speciesResponse] = await Promise.all([
+      fetch(apiUrl),
+      fetch(speciesUrl)
+    ]);
+
+    const pokemonData = await pokemonResponse.json();
+    const speciesData = await speciesResponse.json();
+
+    name.textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+    number.textContent = `#${pokemonData.id.toString().padStart(3, '0')}`;
+    type.textContent = `Type: ${pokemonData.types.map(type => type.type.name).join(', ')}`;
+    height.textContent = `Altura: ${pokemonData.height/10}m`;
+    weight.textContent = `Peso: ${pokemonData.weight/10}kg`;
+    const descriptionEntry = speciesData.flavor_text_entries.find(entry => entry.language.name === "es");
+    description.textContent = descriptionEntry.flavor_text;
+
+    image.style.opacity = 0;
+    image.alt = pokemonData.name;
+    image.src = pokemonData.sprites.other["official-artwork"].front_default;
+    image.style.opacity = 1;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 randomButton.addEventListener('click', getRandomPokemon);
+
