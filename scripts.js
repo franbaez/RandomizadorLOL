242d0448f00d24bc52056e8dec38235f9fcfc364
Franco Baez <francombaez96@gmail.com>
2023-04-13 22:14:43 -0300

    Cambios CSS Card

--- scripts.js
+++ scripts.js
@@ -9,36 +9,44 @@ const weight = card.querySelector('.weight');
 const description = card.querySelector('.description');
 image.style.opacity = 1;
 
-const getRandomPokemon = async () => {
-  const randomId = Math.floor(Math.random() * 898) + 1;
-  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-  const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${randomId}`;
-
+const fetchData = async (url) => {
   try {
-    const [pokemonResponse, speciesResponse] = await Promise.all([
-      fetch(apiUrl),
-      fetch(speciesUrl)
-    ]);
-
-    const pokemonData = await pokemonResponse.json();
-    const speciesData = await speciesResponse.json();
-
-    name.textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
-    number.textContent = `#${pokemonData.id.toString().padStart(3, '0')}`;
-    type.textContent = `Type: ${pokemonData.types.map(type => type.type.name).join(', ')}`;
-    height.textContent = `Altura: ${pokemonData.height/10}m`;
-    weight.textContent = `Peso: ${pokemonData.weight/10}kg`;
-    const descriptionEntry = speciesData.flavor_text_entries.find(entry => entry.language.name === "es");
-    description.textContent = descriptionEntry.flavor_text;
-
-    image.style.opacity = 0;
-    image.alt = pokemonData.name;
-    image.src = pokemonData.sprites.other["official-artwork"].front_default;
-    image.style.opacity = 1;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.log(error);
   }
 };
 
+const getRandomPokemon = async () => {
+  const randomId = Math.floor(Math.random() * 898) + 1;
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+  const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${randomId}`;
+
+  const [pokemonData, speciesData] = await Promise.all([
+    fetchData(pokemonUrl),
+    fetchData(speciesUrl)
+  ]);
+
+  name.textContent = pokemonData.name.toUpperCase();
+  number.textContent = `#${pokemonData.id.toString().padStart(3, '0')}`;
+  type.textContent = `Type: ${pokemonData.types.map(type => type.type.name).join(', ')}`;
+  height.textContent = `Altura: ${pokemonData.height/10}m`;
+  weight.textContent = `Peso: ${pokemonData.weight/10}kg`;
+  const descriptionEntry = speciesData.flavor_text_entries.find(entry => entry.language.name === "es");
+  description.textContent = descriptionEntry.flavor_text;
+
+  image.style.opacity = 0;
+  image.alt = pokemonData.name;
+  image.src = pokemonData.sprites.other["official-artwork"].front_default;
+  image.style.opacity = 1;
+};
+
 randomButton.addEventListener('click', getRandomPokemon);
+
+
+
+
+
 
